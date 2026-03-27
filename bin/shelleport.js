8df7ca14b3b14a8ab6664abc366cefbd4e201e33
Ayaan Zaidi <hi@obviy.us>
2026-03-27 15:01:02 +0530

    chore: prepare 0.0.5 release

--- bin/shelleport.js
+++ bin/shelleport.js
@@ -2,13 +2,19 @@
 
 import { access } from "node:fs/promises";
 import { constants } from "node:fs";
-import { getInstalledBinaryPath, runInstalledBinary } from "../scripts/npm-runtime.js";
+import {
+	getInstalledBinaryPath,
+	installBundle,
+	runInstalledBinary,
+} from "../scripts/npm-runtime.js";
 
-try {
-	await access(getInstalledBinaryPath(), constants.X_OK);
-} catch {
-	console.error("Shelleport runtime missing. Reinstall the package.");
-	process.exit(1);
+async function ensureInstalledBinary() {
+	try {
+		await access(getInstalledBinaryPath(), constants.X_OK);
+	} catch {
+		await installBundle();
+	}
 }
 
+await ensureInstalledBinary();
 runInstalledBinary(process.argv.slice(2));
